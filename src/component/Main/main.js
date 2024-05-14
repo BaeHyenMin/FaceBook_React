684dd7f96b9d1d0660e9eaa12b714ee38313cc34
BaeHyenMin <168337879+BaeHyenMin@users.noreply.github.com>
2024-05-14 19:09:09 +0900

    20240514
    메인헤더 작업

--- src/component/Main/main.js
+++ src/component/Main/main.js
@@ -1,26 +1,27 @@
-import React,{useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
+import '../../css/Main.css';
 
 const Main = () => {
 
-    let [usr, setUsrInfo] = useState([]);
 
-    useEffect(() => {
-        axios.get('/usrView')
-            .then((r) => {
-                console.log(r)
-                setUsrInfo(u =>([...u,...r.data]))})
-    },[])
-
-    return(
-        <div>
-            {usr.map((u,i) => (
-                <div key={i}>
-                    <div>{u.name}</div>
-                </div>
-            ))}
+    return (
+        <div className='flex flex-row mainHeader'>
+            <div className='basis-1/4 headerLeft'>
+                <img src='http://localhost:5123/uploads/facebook_logo.png' alt='이미지없음' />
+                <span>
+                    <img src='http://localhost:5123/search.png' />
+                    <input type='text' placeholder='Facebook검색' />
+                </span>
+            </div>
+            <ul className='basis-1/2 headerCenter'>
+                <li><img src='http://localhost:5123/uploads/facebook_home_before.png' alt='이미지없음' /></li>
+                <li><img src='http://localhost:5123/uploads/facebook_friend_before.png' alt='이미지없음' /></li>
+                <li><img src='http://localhost:5123/uploads/facebook_page_before.png' alt='이미지없음' /></li>
+                <li><img src='http://localhost:5123/uploads/facebook_video_before.png' alt='이미지없음' /></li>
+            </ul>
+            <ul className='basis-1/4 headerRight'>
+                <li><img src='http://localhost:5123/uploads/facebook_message.png' alt='이미지없음' /></li>
+                <li><img src='http://localhost:5123/uploads/facebook_base_profile.png' alt='이미지없음' /></li>
+            </ul>
         </div>
     )
 }
