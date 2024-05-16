2fbc3fec2085b9b2fbc1c8dfe214f5376aa27f61
BaeHyenMin <168337879+BaeHyenMin@users.noreply.github.com>
2024-05-16 18:44:05 +0900

    20240516

--- src/component/Main/main.js
+++ src/component/Main/main.js
@@ -1,10 +1,10 @@
 import '../../css/Main.css';
-
+import '../../css/Home.css'
 const Main = () => {
 
 
     return (
-        <div className='flex flex-row mainHeader'>
+        <div className='flex flex-row main'>
             <div className='basis-1/4 headerLeft'>
                 <img src='http://localhost:5123/uploads/facebook_logo.png' alt='이미지없음' />
                 <span className='search-bar'>
@@ -18,11 +18,19 @@ const Main = () => {
                 <li><img src='http://localhost:5123/uploads/facebook_page_before.png' alt='이미지없음' /></li>
                 <li><img src='http://localhost:5123/uploads/facebook_video_before.png' alt='이미지없음' /></li>
             </ul>
-            <div style={{clear:"both"}}></div>
             <ul className='basis-1/4 headerRight'>
                 <li><img src='http://localhost:5123/uploads/facebook_message.png' alt='이미지없음' /></li>
                 <li><img src='http://localhost:5123/uploads/facebook_base_profile.png' alt='이미지없음' /></li>
             </ul>
+            <div style={{ clear: "both" }}></div>
+            <section className='home'>
+                <div className='card-container'>
+                    <p>계단이 있는 줄 몰랐어요</p>
+                    <video muted autoPlay loop>
+                        <source src="http://localhost:5123/uploads/testvideo.mp4" type="video/mp4" />
+                    </video>
+                </div>
+            </section>
         </div>
     )
 }
