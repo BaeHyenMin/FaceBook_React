99da7abeade04defea07cdf4d79989c8e5ed35ea
BaeHyenMin <168337879+BaeHyenMin@users.noreply.github.com>
2024-05-17 09:38:25 +0900

    20240517

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Main from './component/main/main';
+import Main from '../component/main/main';
 import SignIn from './component/signIn/signIn';
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
