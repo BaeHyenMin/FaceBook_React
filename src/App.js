9d93e4b150697d2767ca2fc3378f6d6bc4aff412
BaeHyenMin <168337879+BaeHyenMin@users.noreply.github.com>
2024-05-22 23:55:14 +0900

    20240522

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-import Main from './component/main/main';
-import SignIn from './component/signIn/signIn';
+// import Main from './component/main/main';
+// import SignIn from './component/signIn/signIn';
+import Main from './component/Main/main'
+import SignIn from './component/SignIn/signIn'
+
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
--- src/component/Main/main.js
+++ src/component/Main/main.js
@@ -34,19 +34,53 @@ const Main = () => {
                     <video controls loop className='video-container'>
                         <source src="http://localhost:5123/uploads/testvideo.mp4" type="video/mp4" />
                     </video>
+                    <div className='card-footer'>
+                        <button><img src='http://localhost:5123/uploads/facebook_favorite_before.png' /> <p>좋아요</p></button>
+                        <button><img src='http://localhost:5123/uploads/comment.png' /> <p>댓글</p></button>
+                        <button><img src='http://localhost:5123/uploads/siren.png' /> <p>신고</p></button>
+                    </div>
                 </div>
+
+
+
                 <div className='card-container'>
+                    <div className='title-container'>
+                        <div className='profile'><img src='http://localhost:5123/uploads/facebook_base_profile.png' /></div>
+                        <p>여기가 제목 들어갈듯?</p>
+                    </div>
+                    <div style={{ clear: "both" }}></div>
                     <p className='content'>계단이 있는 줄 몰랐어요</p>
                     <video controls loop className='video-container'>
                         <source src="http://localhost:5123/uploads/testvideo.mp4" type="video/mp4" />
                     </video>
+                    <div className='card-footer'>
+                        <button><img src='http://localhost:5123/uploads/facebook_favorite_before.png' /> <p>좋아요</p></button>
+                        <button><img src='http://localhost:5123/uploads/comment.png' /> <p>댓글</p></button>
+                        <button><img src='http://localhost:5123/uploads/siren.png' /> <p>신고</p></button>
+                    </div>
                 </div>
+
+
+
+
                 <div className='card-container'>
+                    <div className='title-container'>
+                        <div className='profile'><img src='http://localhost:5123/uploads/facebook_base_profile.png' /></div>
+                        <p>여기가 제목 들어갈듯?</p>
+                    </div>
+                    <div style={{ clear: "both" }}></div>
                     <p className='content'>계단이 있는 줄 몰랐어요</p>
                     <video controls loop className='video-container'>
                         <source src="http://localhost:5123/uploads/testvideo.mp4" type="video/mp4" />
                     </video>
+                    <div className='card-footer'>
+                        <button><img src='http://localhost:5123/uploads/facebook_favorite_before.png' /> <p>좋아요</p></button>
+                        <button><img src='http://localhost:5123/uploads/comment.png' /> <p>댓글</p></button>
+                        <button><img src='http://localhost:5123/uploads/siren.png' /> <p>신고</p></button>
+                        zzz
+                    </div>
                 </div>
+
                 
             </section>
         </div>
